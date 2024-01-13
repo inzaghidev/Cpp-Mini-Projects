dfed288eb2b6ac406a101ceb2aa85794821d5aff
Inzaghi's Dev <139905564+inzaghidev@users.noreply.github.com>
2024-01-13 15:45:07 +0700

    Update perkalian_matriks.cpp

--- input/perkalian_matriks.cpp
+++ input/perkalian_matriks.cpp
@@ -3,53 +3,49 @@ using namespace std;
 
 int main() {
     int matriks1[10][10], matriks2[10][10], hasil[10][10];
-    int i, j, k, m, n, p, q, jumlah = 0;
-    
-    cout<<"Masukkan jumlah Baris Matriks Pertama : ";
-    cin>>m;
-    cout<<"Masukkan jumlah Kolom Matriks Pertama : ";
-    cin>>n;
-    cout<<"Masukkan jumlah Baris Matriks Kedua : ";
-    cin>>p;
-    cout<<"Masukkan jumlah Kolom Matriks Kedua : ";
-    cin>>q;
-    
-    if(n != p){
-      cout<<"Matriks tidak dapat dikalikan satu sama lain.\n";
+    int i, j, k, m, n, jumlah = 0;
+
+    cout << "Program Perkalian Matriks m x m" << endl;
+    cout << "Masukkan jumlah Baris Matriks : ";
+    cin >> m;
+    cout << "Masukkan jumlah Kolom Matriks : ";
+    cin >> n;
+
+    if (n != m) {
+        cout << "Matriks tidak dapat dikalikan satu sama lain." << endl;
     } else {
-        cout<<"Masukkan Elemen Matriks Pertama : \n";
-        for(i = 0; i < m; i++){
-            for(j = 0; j < n; j++){
-                cin>>matriks1[i][j];
+        cout << "Masukkan Elemen Matriks Pertama : " << endl;
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                cin >> matriks1[i][j];
             }
         }
 
-        cout<<"Masukkan Elemen Matriks Kedua : \n";
-        for(i = 0; i < p; i++){
-            for(j = 0; j < q; j++){
-                cin>>matriks2[i][j];
+        cout << "Masukkan Elemen Matriks Kedua : " << endl;
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                cin >> matriks2[i][j];
             }
         }
 
-        for(i = 0; i < m; i++){
-            for(j = 0; j < q; j++){
-                for(k = 0; k < p; k++){
-                  jumlah = jumlah + matriks1[i][k] * matriks2[k][j];
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                for (k = 0; k < m; k++) {
+                    jumlah = jumlah + matriks1[i][k] * matriks2[k][j];
                 }
                 hasil[i][j] = jumlah;
                 jumlah = 0;
             }
         }
-        
-        cout<<"Hasil Perkalian Matriks : \n";
-        for(i = 0; i < m; i++){
-            for(j = 0; j < q; j++){
-                cout<<hasil[i][j]<<"\t";
+
+        cout << "Hasil Perkalian Matriks : " << endl;
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                cout << hasil[i][j] << "\t";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-    
+
     return 0;
 }
- 
